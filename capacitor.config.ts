b6fa140822764eb74e5b5fc850edb582a8b3d34f
danie <dani89rosa@gmail.com>
2025-02-25 12:24:40 +0100

    FIN visualizar android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gramped.letraCifra',
+  appName: 'letraCifra',
+  webDir: 'dist/letra-cifra/browser'
+};
+
+export default config;
