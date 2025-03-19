2ba18e040d5e4ee015d50675267b7dfd71d51f3c
danie <dani89rosa@gmail.com>
2025-03-19 19:15:23 +0100

    optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import './index.css';
+import { Route, Routes } from 'react-router-dom';
 import LetraPage from './pages/LetraPage';
 import CifraPage from './pages/CifraPage';
 import HomePage from './pages/HomePage';
-import './App.css';
-import React from 'react';
 
 function App() {
   return (
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/letraCifra/',  // Asegúrate de que esto coincida con el nombre de tu repositorio
-  build: {
-    outDir: 'dist',  // Asegúrate de que los archivos se generen en la carpeta dist
-  },
 });
+
+
