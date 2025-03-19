e80defdb5581069b02b9465f55c5115f8527dfd2
danie <dani89rosa@gmail.com>
2025-03-19 19:06:14 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import LetraPage from './pages/LetraPage';
 import CifraPage from './pages/CifraPage';
 import HomePage from './pages/HomePage';
-import './App.css'
+import './App.css';
 import React from 'react';
 
-
-
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/letra' element={<LetraPage />} />
-        <Route path='/cifra' element={<CifraPage />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/letra" element={<LetraPage />} />
+      <Route path="/cifra" element={<CifraPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/letraCifra/',  // Asegúrate de que esto coincida con el nombre de tu repositorio
+  build: {
+    outDir: 'dist',  // Asegúrate de que los archivos se generen en la carpeta dist
+  },
+});
